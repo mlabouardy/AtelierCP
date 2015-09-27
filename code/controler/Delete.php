96ec6aaca85798c5fc169c77d5a7fe62dc825d77
mlabouardy <mohamed.labouardy@gmail.com>
2015-09-27 18:50:36 +0200

    update

--- code/controler/Delete.php
+++ code/controler/Delete.php
@@ -4,7 +4,7 @@ require 'config/Connect.php';
 
 $sql_update_workshop = "SELECT * FROM Atelier";
 
-$resultats=$connexion->query($sql_update_workshop);
+$resultats=$db->query($sql_update_workshop);
 $resultats->setFetchMode(PDO::FETCH_OBJ);
 
 while( $resultat = $resultats->fetch() ){
