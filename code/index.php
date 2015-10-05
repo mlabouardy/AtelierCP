7c0e1eb9362b3f1335bf14d6ce6298028c3a2ddf
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-05 21:26:54 +0200

    final version

--- code/index.php
+++ code/index.php
@@ -9,6 +9,7 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular.min.js"></script>
 	<script src="https://code.angularjs.org/1.4.7/angular-route.min.js"></script>
