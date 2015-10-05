f2f9eb059e15405a1e81c3a19846cac7dbc6e6da
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-05 19:07:40 +0200

    update database

--- code/view/assets/js/app.js
+++ code/view/assets/js/app.js
@@ -1,4 +1,21 @@
-var workshopApp=angular.module('workshopApp',[]);
+var workshopApp=angular.module('workshopApp',['ngRoute']);
+
+workshop.config(function($routeProvider){
+	$routeProvider
+		.when('/',
+			  {
+				template:"<h3>ok</h3>",
+				controller:'workshopsCtrl'
+			  }
+		)
+		.when('/sa',
+			  {
+				template:"<h3>ok 2</h3>",
+				controller:'workshopsCtrl'
+			  }
+		)
+		.otherwise({redirectTo:'/'});
+});
 
 workshopApp.controller('workshopsCtrl',function($scope, $http){
 	$http.get("http://51.254.132.239:8081/projet/controler/getWorkshops.php").success(function(data){
