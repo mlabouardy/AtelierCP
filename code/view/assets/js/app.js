c9385616ccd1feaa09e0bdfe469ca514ea4e63f0
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-05 18:53:14 +0200

    update database

--- code/view/assets/js/app.js
+++ code/view/assets/js/app.js
@@ -1,7 +1,7 @@
 var workshopApp=angular.module('workshopApp',[]);
 
 workshopApp.controller('workshopsCtrl',function($scope, $http){
-	$htpp.get().success(function(data){
+	$htpp.get("http://51.254.132.239:8081/projet/controler/getWorkshops.php").success(function(data){
 		$scope.workshops=data;
 	});
 });
